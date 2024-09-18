a7b1964847a2b9fb8fb8f1fe24342f4bc4116004
ekrillik <imstillthinkingofaname@gmail.com>
2024-09-18 09:22:30 +0800

    Added dalaos command to restricted command list

--- cogs/fun.py
+++ cogs/fun.py
@@ -30,6 +30,16 @@ class Fun(commands.Cog):
             embed = discord.Embed(title="This is a pasta.", description="This is a pasta")
             await ctx.send(embed=embed)
 
+    @commands.command()
+    async def dalaos(self, ctx: commands.Context) -> None:
+        if ctx.guild.id not in self.disallowed_server_ids:
+            embed = discord.Embed(
+                title=f"Dalaos",
+                description=f""
+            )
+            embed.add_field(name="PPC", value=f"[Windbell](https://www.youtube.com/@10thwindbell)\n[Fel](https://www.youtube.com/@FelPGR)\n[Acaxi](https://www.youtube.com/@notacaxi)\n[GlobalGlory](https://m.youtube.com/@GlobalGlorypgr)\n[Yor Forger](https://m.youtube.com/@yorforgerpgr)")
+            embed.add_field(name="Warzone", value=f"[sNazz](https://www.youtube.com/@sNazzkun)\n[Empress](https://www.youtube.com/@Oksohee)\n[Setsu](https://www.youtube.com/@Setsugekwa)")
+            await ctx.send(embed=embed)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Fun(bot))
