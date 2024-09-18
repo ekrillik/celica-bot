3e070e4f8116b9de22798a8cf62a2984ea7abbd4
ekrillik <imstillthinkingofaname@gmail.com>
2024-09-18 11:10:35 +0800

    adding Setsu's sheet to bot

--- cogs/help.py
+++ cogs/help.py
@@ -69,7 +69,7 @@ class Help(commands.Cog):
         )
         embed.add_field(name="Officials", value=f"[Website](https://pgr.kurogame.net/)\n[Youtube](https://www.youtube.com/@PunishingGrayRaven)\n[BiliBili](https://space.bilibili.com/382651856)")
         embed.add_field(name="Wikis", value=f"[Gray Ravens](https://grayravens.com/wiki/GRAY_RAVENS)\n[Bili Wiki](https://wiki.biligame.com/zspms/%E9%A6%96%E9%A1%B5)")
-        embed.add_field(name="Competitive Info", value=f"[Huaxu](https://huaxu.app/)")
+        embed.add_field(name="Competitive Info", value=f"[Huaxu](https://huaxu.app/)\n[Setsu's Experimental Spreadsheet](https://docs.google.com/spreadsheets/d/1t7TXDtfZdpHQy8SZAeIMpxcSi1pheSUVgDwkhicCw6s)")
 
         embed.add_field(name="Planning", value=f"[BC Planner](https://docs.google.com/spreadsheets/d/1nNauBXGcHv_7AepLWBLo4ykl3V_EhxpK)")
 
@@ -81,19 +81,6 @@ class Help(commands.Cog):
 
         embed.add_field(name="Outdated Resources", value=f"[Story Reader](https://tf6ksijarp.github.io/)\n[Character 'Tier List by Doomy'](https://docs.google.com/spreadsheets/d/1nCmBq7NstZovPWs9cymAXNyakVXJ4lKvNGbVmtPbcUc)\n[Outdated Advanced PPC Spreadsheet](https://discord.com/channels/595893569609269251/663286717985456157/1283247685255696425)\n[Comprehensive Character Builds](https://docs.google.com/spreadsheets/d/1_NAHdVouSp2T6AwStpz9ZMLZ_ca5EzcuHde5obIlero)\n[Resource Calculator](https://docs.google.com/spreadsheets/d/1rfS6P1UOcZFj_ru2dqLzRkTE39Z0Phjbi5XDsOoNYRs)\n[Coatings Acquisition](https://docs.google.com/spreadsheets/d/1uIWrtp3mZEZgQseY788WHGp7_0mZBE8zSkpVOVCWtP8)")
         await ctx.send(embed=embed)
-
-    # @commands.command(aliases=["contentcreators"])
-    # async def cc(self, ctx: commands.Context):
-    #     embed = discord.Embed(
-    #         title=f"Content Creators",
-    #         description=f""
-    #     )
-    #     embed.add_field(name="General Content", value=f"[Rexlent](https://www.youtube.com/@Rexlent)\n[Narushio](https://www.youtube.com/@narushio)\n[Moteyaba](https://www.youtube.com/@Moteyaba)\n[Spider2B](https://www.youtube.com/@Spider2B)")
-    #     embed.add_field(name="PPC", value=f"[Windbell](https://www.youtube.com/@10thwindbell)\n[Fel](https://www.youtube.com/@FelPGR)\n[Acaxi](https://www.youtube.com/@notacaxi)\n[GlobalGlory](https://m.youtube.com/@GlobalGlorypgr)\n[Yor Forger](https://m.youtube.com/@yorforgerpgr)")
-    #     embed.add_field(name="Warzone", value=f"[sNazz](https://www.youtube.com/@sNazzkun)\n[Empress](https://www.youtube.com/@Oksohee)\n[Setsu](https://www.youtube.com/@Setsugekwa)")
-    #     embed.add_field(name="MMD", value="[KGEzre](http://www.youtube.com/@KGEzre)")
-    #     await ctx.send(embed=embed)
-
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Help(bot))
