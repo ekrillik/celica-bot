938a02c0028d3a3e49ce9fb384165fb0c322614f
ekrillik <imstillthinkingofaname@gmail.com>
2024-10-18 18:44:13 +0800

    adding exaltair server to fun commands

--- cogs/fun.py
+++ cogs/fun.py
@@ -23,6 +23,7 @@ class Fun(commands.Cog):
             854380499643138118, # Fel's Server
             712404390207815761, # Shahrivar's Server
             431234590372265994, # Ew's Server
+            887647011904557068, # Exaltair
         ]
         self.pasta_command_ran = False
         self.bubblewrap_command_ran = False
