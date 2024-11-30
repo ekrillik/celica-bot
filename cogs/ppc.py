dc08822f215b19fda4c1a65f20740db5c37d239a
ekrillik <imstillthinkingofaname@gmail.com>
2024-11-30 15:09:20 +0800

    adding temporary ppc timer timev2

--- cogs/ppc.py
+++ cogs/ppc.py
@@ -47,11 +47,50 @@ class Ppc(commands.Cog):
             embed.add_field(name="", value=f"# You must have some skill issue if you even considered going over 5 minutes for EXPPC. :upside_down:", inline=False)
         await ctx.send(embed=embed)
 
+    #temporary command; will be merged with time command when luna patch drops
+    @commands.hybrid_command(pass_context=True, description="Accepts a difficulty and a time particular time in seconds to display a score for EX-PPC.")
+    async def timev2(self, ctx: commands.Context, difficulty, time_str):
+        max_seconds = 300
+        difficulties = ["knight", "chaos", "hell"]
+        if difficulty not in difficulties:
+            await ctx.send(content=f"{difficulty} is not a valid difficulty!")
+            return
+
+        power = difficulties.index(difficulty) + 2
+        hp_score_multiplier = difficulties.index(difficulty) + 1
+        try:
+            time_sec = min(abs(int(time_str)), max_seconds)
+        except ValueError:
+            content = f"The provided time needs to be a valid number of seconds!"
+            await ctx.send(content=content)
+            return
+
+        minutes, seconds = divmod(time_sec, 60)
+
+        score = self.calculate_score_v2(power, time_sec, hp_score_multiplier)
+
+        embed = discord.Embed(
+            title="",
+            description=f"**{difficulty.title()} {minutes}:{seconds:02}**"
+        )
+        embed.add_field(name="", value=f"Final Score: {score}", inline=False)
+        if time_sec == max_seconds:
+            embed.add_field(name="", value=f"# You must have some skill issue if you even considered going over 5 minutes for EXPPC. :upside_down:", inline=False)
+        await ctx.send(embed=embed)
+
     @staticmethod
     def calculate_score(power, time):
         factor = 2**power
         time = time+1
         score = 21000 * factor - round((74.875 * float(factor) * float(time)) - (0.25 * float(factor) * float(time) * (float(time)-1.0) / 2.0), 0)
+        return int(score)
+
+    @staticmethod
+    def calculate_score_v2(power, time, multiplier):
+        factor = 2**power
+        time = time+1
+        hp_score = 9000 * multiplier
+        score = (21000 * factor - round((74.875 * float(factor) * float(time)) - (0.25 * float(factor) * float(time) * (float(time)-1.0) / 2.0), 0)) + hp_score
         return int(score)
 
     @commands.hybrid_command(pass_context=True, description="Provides info on a particular boss")
