bd571cb2e98e6a4bbb85a3ddd59ea7883316ce5c
ekrillik <imstillthinkingofaname@gmail.com>
2025-01-10 08:37:28 +0800

    updating PPC command + removing teddy build typo

--- cogs/ppc.py
+++ cogs/ppc.py
@@ -18,36 +18,6 @@ class Ppc(commands.Cog):
     async def on_ready(self):
         print('PPC loaded.')
 
-    @commands.hybrid_command(pass_context=True, aliases=['PPC', 'ppc'], description="Accepts a difficulty and a time particular time in seconds to display a score for EX-PPC.")
-    async def time(self, ctx: commands.Context, difficulty, time_str):
-        max_seconds = 300
-        difficulties = ["test", "elite", "knight", "chaos", "hell"]
-        if difficulty not in difficulties:
-            await ctx.send(content=f"{difficulty} is not a valid difficulty!")
-            return
-
-        power = difficulties.index(difficulty)
-        try:
-            time_sec = min(abs(int(time_str)), max_seconds)
-        except ValueError:
-            content = f"The provided time needs to be a valid number of seconds!"
-            await ctx.send(content=content)
-            return
-
-        minutes, seconds = divmod(time_sec, 60)
-
-        score = self.calculate_score(power, time_sec)
-
-        embed = discord.Embed(
-            title="",
-            description=f"**{difficulty.title()} {minutes}:{seconds:02}**"
-        )
-        embed.add_field(name="", value=f"Final Score: {score}", inline=False)
-        if time_sec == max_seconds:
-            embed.add_field(name="", value=f"# You must have some skill issue if you even considered going over 5 minutes for EXPPC. :upside_down:", inline=False)
-        await ctx.send(embed=embed)
-
-    #temporary command; will be merged with time command when luna patch drops
     @commands.hybrid_command(pass_context=True, description="Accepts a difficulty and a time particular time in seconds to display a score for EX-PPC.")
     async def timev2(self, ctx: commands.Context, difficulty, time_str):
         max_seconds = 300
