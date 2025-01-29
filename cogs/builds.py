4174902f28d86599a65b1f2e24f0fb76b8622ca6
Bryan K <97289449+E-kun@users.noreply.github.com>
2025-01-29 17:06:14 +0800

    switching affixteams command to cnteams

--- cogs/builds.py
+++ cogs/builds.py
@@ -41,7 +41,7 @@ class Builds(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.hybrid_command(aliases=['AffixTeams', 'Affixteams', 'at'], description="Displays the affix teams infographic by Aethervoid used to explain affix teams.")
-    async def affixteams(self, ctx: commands.Context):
+    async def cnteams(self, ctx: commands.Context):
         embed = discord.Embed(title="", description="")
         embed.set_image(url='https://pgr-discord-bot.s3.ap-southeast-2.amazonaws.com/Infographics/Affix_Teams_v2.png')
         await ctx.send(embed=embed)
